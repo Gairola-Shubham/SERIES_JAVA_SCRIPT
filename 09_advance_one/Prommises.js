be3089385fed7bf98ffe6d6514ac14ecbecb0447
Gairola-Shubham <shubhamgairola124@gmail.com>
2025-02-16 00:01:27 +0530

    initial commit

---
+++ 09_advance_one/Prommises.js
@@ -0,0 +1,7 @@
+// error 404, we will get it at response 
+// fetch()   two parts
+// web browser/ node (network request :- responsed or reject)
+// data:-  OnField[]  onRejection[]  array me direct input is not allowed
+
+
+console.log("promises");
